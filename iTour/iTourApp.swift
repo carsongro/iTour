002f3879f816082ed5111c2f9c09f1a63fa4f4bd
Carson Gross <carsongro@gmail.com>
2023-11-01 18:25:59 -0600

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Carson Gross on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
